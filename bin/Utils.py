354dd2a0bce3b48460bf9e3b80fbc19a188bd795
pushebp <pushebp@163.com>
2017-07-13 09:49:44 +0800

    1

--- bin/Utils.py
+++ bin/Utils.py
@@ -6,14 +6,14 @@ import tempfile
 from subprocess import check_output, CalledProcessError
 import subprocess
 
-# 返回当前脚本的全路径，末尾带\
+# 返回当前脚本的全路径，末尾不带\
 def getthispath():
     path = sys.path[0]
     #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
     if os.path.isdir(path):
-        return path + os.sep
+        return path
     elif os.path.isfile(path):
-        return os.path.split(path)[0] + os.sep
+        return os.path.split(path)[0]
 
 def get_file_ext(path):
     print os.path.splitext(path)
